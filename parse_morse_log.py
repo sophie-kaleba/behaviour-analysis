3983aa300b3ec9f2bd84380e2601b499d0fff7d8
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-01-20 13:26:32 +0000

    add the parsing of the splitting summary

--- parse_morse_log.py
+++ parse_morse_log.py
@@ -1,19 +1,26 @@
 import re
-import os
 import sys
 
 fin = sys.argv[1]
 f_out = sys.argv[2]
+split_summary = "split_"+f_out
 
 fout = open(f_out, 'wt')
+split_fout = open(split_summary, 'wt')
+
 
 with open(fin) as fp:
    line = fp.readline()
-   while "INFO: " not in line :
+   while "INFO: " not in line : #ignore extraneous log at the beggining of the file
        line = fp.readline()
-   while line:
+   while "INFO: " in line : #info is contained in line
        line = re.sub(r'^.*?:', ' ', line)
        fout.write(line)
        line = fp.readline()
+   if "Splitting Statistics" in line : #starting the splitting summary 
+       while line:
+        split_fout.write(line)
+        line = fp.readline()
 
 fout.close()
+split_fout.close()
