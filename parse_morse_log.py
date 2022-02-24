1cbf7e050a7a1e03d4e6d54afc4b592833532473
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-02-24 15:33:13 +0000

    fix the parsing when a splitting summary is available

--- parse_morse_log.py
+++ parse_morse_log.py
@@ -10,17 +10,22 @@ split_fout = open(split_summary, 'wt')
 
 
 with open(fin) as fp:
-   line = fp.readline()
-   while "INFO: " not in line : #ignore extraneous log at the beggining of the file
-       line = fp.readline()
-   while "INFO: " in line : #info is contained in line
-       line = re.sub(r'^.*?:', ' ', line)
-       fout.write(line)
-       line = fp.readline()
-   if "Splitting Statistics" in line : #starting the splitting summary 
-       while line:
-        split_fout.write(line)
+    while True:
         line = fp.readline()
+        if ("INFO: " not in line) and ("Splitting Statistics" not in line) : #ignore extraneous log at the beggining of the file
+            line = fp.readline()
+        if "INFO: " in line : #info is contained in line
+            line = re.sub(r'^.*?:', ' ', line)
+            fout.write(line)
+            #line = fp.readline()
+        if "Splitting Statistics" in line : #starting the splitting summary 
+            while True:
+                split_fout.write(line)
+                line = fp.readline()
+                if not line:
+                    break
+        if not line:
+                    break
 
 fout.close()
 split_fout.close()
