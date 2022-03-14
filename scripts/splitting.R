2e33cda2ecb879b2e6af691d3be431fa357a913b
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-03-14 20:08:09 +0000

    refine splitting analysis

--- scripts/splitting.R
+++ scripts/splitting.R
@@ -16,8 +16,12 @@ analyse_splitting_transitions <- function(df_p, benchmark_name) {
   #prepare the data frame for analysis
   raw_splitting <- df_p %>%
     group_by_at(Call.Site) %>%
-    dplyr::mutate(Times.splitted = n_distinct(CT.Address) - 1, Num.Calls = n_distinct(Call.ID))
-  raw_splitting <- raw_splitting %>% dplyr::select(Call.ID, Symbol, Source.Section, CT.Address, Observed.Receiver, Benchmark, Num.Receiver.Observed, Cache.Type.Observed, Times.splitted, Num.Calls)
+    dplyr::mutate(Times.splitted = n_distinct(CT.Address) - 1, Num.Calls = n_distinct(Call.ID)) %>%
+    ungroup() %>%
+    group_by_at(Call.Site.Target) %>%
+    dplyr::mutate(Num.Calls.Target = n_distinct(Call.ID))
+
+  raw_splitting <- raw_splitting %>% dplyr::select(Call.ID, Symbol, Source.Section, CT.Address, Observed.Receiver, Benchmark, Num.Receiver.Observed, Cache.Type.Observed, Times.splitted, Num.Calls.Target)
 
   # and export it as a csv file so our java program can analyse it
   split_file <-  paste(getwd(),"/",benchmark_name,"splitting_data.csv", sep="")
@@ -28,13 +32,13 @@ analyse_splitting_transitions <- function(df_p, benchmark_name) {
   command <- paste("cd ./splitting-transition/out/production/splitting-transition/ ; java CallSiteAnalyzer ", split_file, " ", out_split_file, sep="")
   system(command)  
 
-  row_names <- c("Source.Section", "Symbol", "Start.ID", "End.ID", "Start.State", "End.State", "Start.Cache.Size", "End.Cache.Size", "Union.Size", "Intersect.Size", "Benchmark")
+  row_names <- c("Source.Section", "Symbol", "Start.ID", "End.ID", "Start.State", "End.State", "Start.Cache.Size", "End.Cache.Size", "Union.Size", "Intersect.Size", "Benchmark", "Num.Calls.Target")
   transition_data <- read.csv(out_split_file, header = FALSE, sep = ",", row.names=NULL, col.names=row_names) 
   return(transition_data)
 }
 
 
-summarise_transition <- function(df_p) {
+summarise_transition_sites <- function(df_p) {
   transition_data <- df_p %>%
     dplyr::mutate(Transition.Type = case_when( Start.State == 'POLYMORPHIC' & End.State == 'POLYMORPHIC' & Intersect.Size == Union.Size ~ "Same",
                                              Start.State == 'MONOMORPHIC' & End.State == 'MONOMORPHIC' & Intersect.Size == Union.Size ~ "Same",
@@ -49,7 +53,25 @@ summarise_transition <- function(df_p) {
     tidyr::spread(Transition, Num.Transitions)
 
   return(transition_data)
+}
 
+summarise_transition_frequency <- function(df_p, type_calls) {
+  transition_data <- df_p %>%
+    dplyr::mutate(Transition.Type = case_when( Start.State == 'POLYMORPHIC' & End.State == 'POLYMORPHIC' & Intersect.Size == Union.Size ~ "Same",
+                                             Start.State == 'MONOMORPHIC' & End.State == 'MONOMORPHIC' & Intersect.Size == Union.Size ~ "Same",
+                                             Start.State == 'MEGAMORPHIC' & End.State == 'MEGAMORPHIC' & Intersect.Size == Union.Size ~ "Same",
+                                             TRUE ~ "Different")) %>%
+    group_by(Benchmark, Start.State, End.State, Transition.Type) %>%
+    dplyr::summarise(Num.Transitions = n()) %>%
+    dplyr::mutate(Frequency = round(Num.Transitions/type_calls,7)*100) %>%
+    dplyr::select(-Num.Transitions)
+
+  transition_data <- transition_data %>% 
+    tidyr::unite("Transition", Start.State:End.State, remove = TRUE, sep=" -> ") %>%
+    tidyr::unite("Transition", Transition:Transition.Type, remove = TRUE, sep="_") %>%
+    tidyr::spread(Transition, Frequency)
+
+  return(transition_data)
 }
 
 
