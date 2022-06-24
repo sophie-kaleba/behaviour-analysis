07a4209e84246d1190bc8983d606c6dd31fe6914
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-06-24 01:44:54 +0100

    add the covs script and update the header in the merge table script

---
+++ cov_folder/coverage_runner.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+TRUBY=("Acid"
+	  "AsciidoctorConvertSmall" 
+	  "AsciidoctorLoadFileSmall"
+	  "ImageDemoConv"
+	  "ImageDemoSobel"
+	  "OptCarrot"
+	  "MatrixMultiply"
+	  "Pidigits"
+	  "RedBlack"
+	  "SinatraHello"
+)
+
+AWFY=("BinaryTrees"
+	  "Bounce"
+	  #"CD" see bottom
+	  "DeltaBlue" 
+	  "FannkuchRedux"
+	  #"Havlak" see bottom
+	  "Json"
+	  "LeeBench"
+	  "List"
+	  "Mandelbrot"
+	  "NBody"
+	  "NeuralNet"
+	  "Permute"
+	  "PsychLoad"
+	  "Queens"
+	  "Richards"
+	  "Sieve"
+	  "SpectralNorm"
+	  "Storage"
+	  "Towers"
+)
+
+YJIT=("HexaPdfSmall"
+	  "LiquidCartParse" 
+	  "LiquidCartRender" 
+	  "LiquidMiddleware"
+	  "LiquidParseAll"
+	  "LiquidRenderBibs"
+	  "MailBench"
+	  "RubykonBench"
+)
+
+RAILS=("BlogRailsRoutesTwoRoutesTwoRequests"
+	   "ERubiRails" 
+)
+
+ CHUNKY=("ChunkyCanvasResamplingBilinear"
+	    "ChunkyCanvasResamplingNearestNeighbor"
+		"ChunkyCanvasResamplingSteps"
+		"ChunkyCanvasResamplingStepsResidues"
+		"ChunkyColorA"
+		"ChunkyColorB"
+		"ChunkyColorComposeQuick"
+		"ChunkyColorG"
+		"ChunkyColorR"
+		"ChunkyDecodePngImagePass"
+		"ChunkyOperationsCompose"
+		"ChunkyOperationsReplace"
+)
+
+PSD=(
+	"PsdColorCmykToRgb"
+	 "PsdComposeColorBurn"
+	 "PsdComposeColorDodge"
+	 "PsdComposeDarken"
+	 "PsdComposeDifference"
+	 "PsdComposeExclusion"
+	 "PsdComposeHardLight"
+	 "PsdComposeHardMix"
+	 "PsdComposeLighten"
+	 "PsdComposeLinearBurn"
+	 "PsdComposeLinearDodge"
+	 "PsdComposeLinearLight"
+	 "PsdComposeMultiply"
+	 "PsdComposeNormal"
+	 "PsdComposeOverlay"
+	 "PsdComposePinLight"
+	 "PsdComposeScreen"
+	 "PsdComposeSoftLight"
+	 "PsdComposeVividLight"
+	 "PsdImageformatLayerrawParseRaw"
+	 "PsdImageformatRleDecodeRleChannel"
+	 "PsdImagemodeCmykCombineCmykChannel"
+	 "PsdImagemodeGreyscaleCombineGreyscaleChannel"
+	 "PsdImagemodeRgbCombineRgbChannel"
+	 "PsdRendererBlenderCompose"
+	 "PsdRendererClippingmaskApply"
+	 "PsdRendererMaskApply"
+	 "PsdUtilClamp"
+	 "PsdUtilPad2"
+	 "PsdUtilPad4"
+)
+
+# for b in ${TRUBY[@]}; do
+# 	make benchmark_name=$b iterations="1" inner_iterations="1" FOLDER=$FOLDER all
+# 	wait $!
+# done
+
+# for b in ${AWFY[@]}; do
+# 	make benchmark_name=$b iterations="1" inner_iterations="1" FOLDER=$FOLDER all
+# 	wait $!
+# done
+
+# for b in ${YJIT[@]}; do
+# 	make benchmark_name=$b iterations="1" inner_iterations="1" FOLDER=$FOLDER all
+# 	wait $!
+# done
+
+# for b in ${RAILS[@]}; do
+# 	make benchmark_name=$b iterations="1" inner_iterations="1" FOLDER=$FOLDER all
+# 	wait $!
+# done
+
+# for b in ${CHUNKY[@]}; do
+# 	make benchmark_name=$b iterations="1" inner_iterations="1" FOLDER=$FOLDER all
+# 	wait $!
+# done
+
+# for b in ${PSD[@]}; do
+# 	make benchmark_name=$b iterations="1" inner_iterations="1" FOLDER=$FOLDER all
+# 	wait $!
+# done
+
+#must have more memory
+make benchmark_name="Havlak" iterations="1" inner_iterations="1" FLAGS="--vm.Xss6m" FOLDER=$FOLDER all
+
+# is special regarding the number of inner iterations
+# make benchmark_name="CD" iterations="1" inner_iterations="250" FOLDER=$FOLDER all
