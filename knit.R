e7e4272ba1cb285f5859383296c1c59203531939
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-07-21 22:25:06 +0100

    clean generate report script and update knitr file to pass the relevant variables

--- knit.R
+++ knit.R
@@ -2,10 +2,8 @@
 library(knitr);
 args    <- commandArgs(trailingOnly = TRUE)
 # the following variables are shared with the knitted r-script
-filename = args[3]
-benchmark_name = args[4]
-number_iterations = args[5]
-number_inner_iterations = args[6]
-flags_cmd = args[7]
-folder_out = args[8]
+folder_status = args[3]
+folder_cov = args[4]
+folder_report = args[5]
+
 silence <- knit(args[1], args[2]);
