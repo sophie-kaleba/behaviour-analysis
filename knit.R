3e2a08280af86e2948f4acee0afc57b8ed9dffdc
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-08-01 22:53:43 +0100

    take blocks and startup into account

--- knit.R
+++ knit.R
@@ -2,9 +2,14 @@
 library(knitr);
 args    <- commandArgs(trailingOnly = TRUE)
 # the following variables are shared with the knitted r-script
-folder_status = args[3]
-folder_cov = args[4]
-folder_report = args[5]
-folder_details = args[6]
+folder_cov = args[3] #input for coverage
+
+folder_status = args[4] #input for global data
+folder_details = args[5] #input for detailed data
+
+folder_report = args[6] #output for latex tables and pdf report
+
+keep_startup = as.logical(args[7])
+keep_blocks = as.logical(args[8])
 
 silence <- knit(args[1], args[2]);
