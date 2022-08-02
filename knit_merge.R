e9de77f26d2b77b694fc86a155c81f7b96771bef
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-08-02 13:29:13 +0100

    delete unused files

--- knit_merge.R
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env Rscript
-library(knitr);
-args    <- commandArgs(trailingOnly = TRUE)
-# the following variables are shared with the knitted r-script
-folder_out = args[3]
-silence <- knit(args[1], args[2]);
