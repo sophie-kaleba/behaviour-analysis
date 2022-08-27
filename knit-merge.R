be91e2579751433cd0a84cf480ceaa2bcce93009
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-08-27 14:18:39 +0100

    [WIP] merging is ok, still need to build startup tables

---
+++ knit-merge.R
@@ -0,0 +1,8 @@
+#!/usr/bin/env Rscript
+library(knitr);
+args    <- commandArgs(trailingOnly = TRUE)
+# the following variables are shared with the knitted r-script
+folder_status = args[3] #input for coverage
+folder_path = args[4] #input for global data
+
+silence <- knit(args[1], args[2]);
