b0ae95bae2390d6b80c5ddee48d008a85cf70aab
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-10-03 18:26:58 +0100

    add path for blocks in knit script

--- knit-merge.R
+++ knit-merge.R
@@ -4,5 +4,6 @@ args    <- commandArgs(trailingOnly = TRUE)
 # the following variables are shared with the knitted r-script
 folder_status = args[3] #input for coverage
 folder_path = args[4] #input for global data
+folder_blocks = args[5]
 
 silence <- knit(args[1], args[2]);
